72a3f5b00166e45839fa9c4e35a455e523007e78
cxlk <1980757775@qq.com>
2022-07-24 11:52:42 +0800

    项目初始化

---
+++ src/main/java/com/excel/asyncexcel/AsyncExcelApplication.java
@@ -0,0 +1,13 @@
+package com.excel.asyncexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excel/asyncexcel/AsyncExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excel.asyncexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
